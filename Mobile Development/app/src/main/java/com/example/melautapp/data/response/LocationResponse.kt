b51b3f55f5e378a4ccc6a687fd90ece5c496ab24
intandewihapsari <intandewihapsarii@gmail.com>
2024-12-08 00:24:55 +0700

    update suhu

--- Mobile Development/app/src/main/java/com/example/melautapp/data/response/LocationResponse.kt
+++ Mobile Development/app/src/main/java/com/example/melautapp/data/response/LocationResponse.kt
@@ -1,5 +1,7 @@
 package com.example.melautapp.data.response
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class LocationResponse(
@@ -20,4 +22,37 @@ data class LocationResponse(
 
 	@field:SerializedName("lat")
 	val lat: String
-)
+): Parcelable {
+	constructor(parcel: Parcel) : this(
+		parcel.readString().toString(),
+        parcel.readString().toString(),
+		parcel.readString().toString(),
+		parcel.readString().toString(),
+		parcel.readString().toString(),
+		parcel.readString().toString(),
+	) {
+	}
+
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeString(id)
+		parcel.writeString(propinsi)
+		parcel.writeString(kota)
+		parcel.writeString(kecamatan)
+		parcel.writeString(lat)
+		parcel.writeString(lon)
+	}
+
+	override fun describeContents(): Int {
+		return 0
+	}
+
+	companion object CREATOR : Parcelable.Creator<LocationResponse> {
+		override fun createFromParcel(parcel: Parcel): LocationResponse {
+			return LocationResponse(parcel)
+		}
+
+		override fun newArray(size: Int): Array<LocationResponse?> {
+			return arrayOfNulls(size)
+		}
+	}
+}
